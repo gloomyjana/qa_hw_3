38bd31863868cc0819a9bace7010a2a9f68a4a85
Jana <yanabling@gmail.com>
2023-03-04 20:27:43 +0700

    test is done

---
+++ src/test/java/StudentRegistrationFormTests.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+
+public class StudentRegistrationFormTests {
+
+    @Test
+    void fillRegistrationFormTest() {
+        open("https://demoqa.com/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#firstName").setValue("John");
+        $("#lastName").setValue("Doe");
+        $("#userEmail").setValue("johndoe@gmail.com");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("5553331234");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-dropdown-container").$(byText("1990")).click();
+        $(".react-datepicker__month-dropdown-container").$(byText("April")).click();
+        $(".react-datepicker__month-container").$(byText("22")).click();
+        $("#subjectsInput").setValue("Arts").pressEnter();
+        $("#subjectsInput").setValue("Physics").pressEnter();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/pictures/pic.jpg"));
+        $("#currentAddress").setValue("Current Address");
+        $("#state").click();
+        $("#react-select-3-option-3").click();
+        $("#city").click();
+        $("#react-select-4-option-1").click();
+        $("#submit").click();
+        sleep(5000);
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("John Doe"), text("johndoe@gmail.com"), text("Male"),
+                text("Arts, Physics"), text("Music"), text("pic.jpg"), text("Rajasthan Jaiselmer"));
+
+        $("#closeLargeModal").click();
+    }
+}
