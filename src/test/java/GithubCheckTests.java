71202b4615100f1a37bc0acb4e53faa9e7068913
Jana <yanabling@gmail.com>
2023-03-05 19:26:00 +0700

    added codeExample

--- src/test/java/GithubCheckTests.java
+++ src/test/java/GithubCheckTests.java
@@ -7,12 +7,24 @@ import static com.codeborne.selenide.Selenide.*;
 public class GithubCheckTests {
     @Test
     void checkJUnit5CodeExampleTest() {
+        String codeExample = "@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}";
+
         open("https://github.com/selenide/selenide");
 
         $("#wiki-tab").click();
         $(".wiki-more-pages-link").$("button").click();
         $(".Box--condensed").shouldHave(text("SoftAssertions"));
         $(byText("SoftAssertions")).click();
-        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+        $(".markdown-body").shouldHave(text(codeExample));
     }
 }
